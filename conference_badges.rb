3260c138ae53b0db8d4b5daa4eb774e629cc0117
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-11 19:39:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,17 @@
 # Write your code here.
 def badge_maker(name)
   
-  puts "Hello, my name is #{name}"
+  "Hello, my name is #{name}"
+  
+end
+
+def batch_badge_creator(attendees)
+  
+  attendees.each do |name|
+    
+    list << badge_maker(name)
+    
+  end 
+  list
   
 end
